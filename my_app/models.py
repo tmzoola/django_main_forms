1bee152cd6e325ab6813c67a6cd993127f5ec4b2
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-02-27 19:37:24 +0900

    Changes in model

--- my_app/models.py
+++ my_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
 
@@ -10,4 +11,4 @@ class Review(models.Model):
 
 class Car(models.Model):
     name = models.CharField(max_length=100)
-    year = models.IntegerField()
+    year = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
